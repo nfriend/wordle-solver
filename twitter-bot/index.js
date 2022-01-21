04b47bb5409397333b29936a82ea39294a3e5d07
Nathan Friend <hello@nathanfriend.io>
2022-01-21 13:20:26 -0500

    Generate own share text

--- twitter-bot/index.js
+++ twitter-bot/index.js
@@ -4,13 +4,10 @@ const asyncTimeout = (timeout) =>
   new Promise((resolve) => setTimeout(() => resolve(), timeout));
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const context = browser.defaultBrowserContext();
-  context.overridePermissions('https://www.powerlanguage.co.uk', [
-    'clipboard-read',
-    'clipboard-write',
-  ]);
   const page = await browser.newPage();
+  await page.emulateTimezone('GMT');
   await page.goto('https://www.powerlanguage.co.uk/wordle/', {
     waitUntil: 'networkidle2',
   });
@@ -38,21 +35,45 @@ const asyncTimeout = (timeout) =>
     });
   });
 
-  await asyncTimeout(5000);
+  // Interacting with the clipboard seems challenging/impossible
+  // within Puppeteer, so instead we'll just manually generate
+  // our own share text.
+  const shareText = await page.evaluate((_) => {
+    const gameState = getGameState();
 
-  await page.evaluate((_) => {
-    document
-      .querySelector('game-app')
-      .shadowRoot.querySelector('game-stats')
-      .shadowRoot.querySelector('button#share-button')
-      .click();
+    const results = [];
+
+    const guessCount = gameState.guesses.length;
+
+    // These three lines are more or less copied from Wordle's source
+    const s = new Date('2021-06-19');
+    const t = new Date().setHours(0, 0, 0, 0) - s.setHours(0, 0, 0, 0);
+    const wordleCount = Math.round(t / 864e5);
+
+    results.push(`Wordle ${wordleCount} ${guessCount}/6`);
+    results.push('');
+    gameState.guesses.forEach((guess) => {
+      const resultLine = guess.letters
+        .map((letter) => {
+          if (letter.evaluation === 'correct') {
+            return '🟩';
+          } else if (letter.evaluation === 'present') {
+            return '🟨';
+          } else {
+            return '⬜';
+          }
+        })
+        .join('');
+
+      results.push(resultLine);
+    });
+
+    return results.join('\n');
   });
 
-  await asyncTimeout(3000);
+  console.log('shareText:', shareText);
 
-  const shareText = await page.evaluate((_) => navigator.clipboard.readText());
-
-  console.log('share text:', shareText);
+  // TODO: Tweet results here
 
   await browser.close();
 })();
